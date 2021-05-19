9a97575d983c3848df27badc02077dabd118d510
Ricardenho <andrade.comim@gmail.com>
2021-05-18 21:29:03 -0300

    exe 043

---
+++ exe043.js
@@ -0,0 +1,8 @@
+//Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+for(var i = 0;i<=50; i++){
+    if(i%2==0){
+        console.log(i)
+    }
+    
+}
